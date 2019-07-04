d3efc2525ec3089ac0aaec54558a5016388d19d6
YdeaGames <ydeagames@gmail.com>
2019-07-04 14:50:28 +0900

    GameObjects

--- GameObjects/Game/BaseScene.cpp
+++ GameObjects/Game/BaseScene.cpp
@@ -13,15 +13,57 @@ using namespace DirectX::SimpleMath;
 // ¶¬
 void BaseScene::Build(GameContext& context)
 {
-	/*
-	context.GetCamera().view = Matrix::CreateLookAt(Vector3(0, 5, 10), Vector3::Zero, Vector3::Up);
+	auto debugCamera  = GameObject::Create();
+	debugCamera->AddComponent<DebugCameraWrapper>();
+	context << debugCamera;
 
-	auto bit = std::make_shared<GeometricObject>(
-			[](GameContext& context) { return GeometricPrimitive::CreateTeapot(context.GetDR().GetD3DDeviceContext()); },
-			Color(Colors::Blue)
-		);
-	context << bit;
-	*/
+	auto gridFloor = GameObject::Create();
+	gridFloor->AddComponent<GridFloorWrapper>();
+	context << gridFloor;
 
-	context.GetSceneManager().LoadScene("LogoScene");
+	class BoxBehaviour : public Component
+	{
+		void Update(GameContext& context)
+		{
+			gameObject->transform->LocalPosition = Vector3::Transform(Vector3::Right, Matrix::CreateRotationY(float(context.GetTimer().GetTotalSeconds()))) * 4;
+		}
+	};
+	auto box = GameObject::Create();
+	box->AddComponent<GeometricObject>([](GameContext& ctx) { return GeometricPrimitive::CreateCube(ctx.GetDR().GetD3DDeviceContext()); });
+	box->AddComponent<BoxBehaviour>();
+	//box->transform->SetPosition(Vector3(2, 0, 0));
+	context << box;
+
+	class SphereBehaviour : public Component
+	{
+		void Update(GameContext& context)
+		{
+			auto input = Vector3::Zero;
+			if (Input::GetKey(Keyboard::Keys::A) || Input::GetKey(Keyboard::Keys::Left))
+				input.x -= 1;
+			if (Input::GetKey(Keyboard::Keys::D) || Input::GetKey(Keyboard::Keys::Right))
+				input.x += 1;
+			if (Input::GetKey(Keyboard::Keys::W) || Input::GetKey(Keyboard::Keys::Up))
+				input.z -= 1;
+			if (Input::GetKey(Keyboard::Keys::S) || Input::GetKey(Keyboard::Keys::Down))
+				input.z += 1;
+			if (Input::GetKey(Keyboard::Keys::Q) || Input::GetKey(Keyboard::Keys::LeftShift))
+				input.y -= 1;
+			if (Input::GetKey(Keyboard::Keys::E) || Input::GetKey(Keyboard::Keys::Space))
+				input.y += 1;
+
+			Vector3 s_, t_;
+			Quaternion rotation;
+			context.GetCamera().view.Decompose(s_, rotation, t_);
+			rotation.Inverse(rotation);
+
+			auto force = Vector3::Transform(input, rotation);
+			gameObject->transform->LocalPosition += force;
+		}
+	};
+	auto sphere = GameObject::Create();
+	sphere->AddComponent<GeometricObject>([](GameContext& ctx) { return GeometricPrimitive::CreateSphere(ctx.GetDR().GetD3DDeviceContext()); });
+	sphere->AddComponent<SphereBehaviour>();
+	sphere->transform->SetPosition(Vector3(-1, 0, 3));
+	context << sphere;
 }
--- GameObjects/Game/BuildSettings.cpp
+++ GameObjects/Game/BuildSettings.cpp
@@ -2,22 +2,13 @@
 #include "BuildSettings.h"
 #include <Framework/SceneManager.h>
 #include "BaseScene.h"
-#include "LogoScene.h"
-#include "TitleScene.h"
-#include "PlayScene.h"
-#include "ResultScene.h"
 
-const std::wstring BuildSettings::GAME_TITLE = L"カタトウス";
-const std::wstring BuildSettings::GAME_TITLE_CLASS = L"KatatousWindowClass";
+const std::wstring BuildSettings::GAME_TITLE = L"GameObjects";
+const std::wstring BuildSettings::GAME_TITLE_CLASS = L"GameObjectsWindowClass";
 
 void BuildSettings::Build(GameContext& context)
 {
 	context.GetSceneManager().Register<BaseScene>("BaseScene");
-	context.GetSceneManager().Register<LogoScene>("LogoScene");
-	context.GetSceneManager().Register<TitleScene>("TitleScene");
-	context.GetSceneManager().Register<PlayScene>("PlayScene");
-	context.GetSceneManager().Register<ResultScene>("ResultScene");
 
-	//context.GetSceneManager().LoadScene("BaseScene");
-	context.GetSceneManager().LoadScene("PlayScene");
+	context.GetSceneManager().LoadScene("BaseScene");
 }
