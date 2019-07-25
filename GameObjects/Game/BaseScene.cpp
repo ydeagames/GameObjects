8e984dcf27626a4861d4c052e0acd06702187177
YdeaGames <ydeagames@gmail.com>
2019-07-25 14:41:14 +0900

    Collision

--- GameObjects/Game/BaseScene.cpp
+++ GameObjects/Game/BaseScene.cpp
@@ -59,6 +59,7 @@ void BaseScene::Build(GameContext& context)
 				time = 0;
 
 				auto bullet = std::make_shared<BulletBehaviour>(transform);
+				bullet->layer = Layer::LAYER_BALL;
 				bullet->transform->localPosition = Vector3(Random::Range(-4.f, 4.f), 0, Random::Range(-4.f, 4.f));
 				bullet->vel = Vector3::Transform(Vector3::Forward, Matrix::CreateFromAxisAngle(Vector3::UnitY, Random::Range(0.f, XM_2PI))) * .1f;
 				context << bullet;
@@ -68,6 +69,67 @@ void BaseScene::Build(GameContext& context)
 
 	auto gen = std::make_shared<BulletGenerator>();
 	context << gen;
+
+	class CollisionController : public GameObject
+	{
+		void Update(GameContext& context)
+		{
+			auto& scene = context.GetScene();
+			std::vector<std::pair<std::list<std::shared_ptr<GameObject>>&, std::list<std::shared_ptr<GameObject>>&>> collisionMatrix =
+			{
+				{ scene.gameObjects[Layer::LAYER_BALL], scene.gameObjects[Layer::LAYER_BALL] },
+				{ scene.gameObjects[Layer::LAYER_PLAYER], scene.gameObjects[Layer::LAYER_BALL] },
+			};
+
+			for (auto& col : collisionMatrix)
+				for (auto& obj1i : col.first)
+					for (auto& obj2i : col.second)
+					{
+						float radius = .8f;
+						if (obj1i < obj2i && Vector3::DistanceSquared(obj1i->transform->localPosition, obj2i->transform->localPosition) < (radius + radius) * (radius + radius))
+						{
+							auto obj1 = std::dynamic_pointer_cast<BulletBehaviour>(obj1i);
+							auto obj2 = std::dynamic_pointer_cast<BulletBehaviour>(obj2i);
+							if (obj1 && obj2)
+							{
+								// 位置の差のベクトルに対して垂直なベクトルと水平な方向に速度ベクトルを分解
+								auto subpos = obj2->transform->localPosition -obj1->transform->localPosition;	// 位置の差
+								auto subvel = obj2->vel - obj1->vel;	// 速度の差
+								auto subposNorm = subpos;	// 位置の差単位ベクトル
+								auto subvelNorm = subvel;	// 速度の差単位ベクトル
+								subposNorm.Normalize();
+								subvelNorm.Normalize();
+
+								// 先生のArrow.cpp
+								auto rotAngle = std::acos(subposNorm.Dot(subvelNorm));
+								auto rotAxis = subposNorm.Cross(subvelNorm);
+								if (rotAxis.Length() == 0.0f) rotAxis = Vector3(0.0f, 0.0f, 1.0f);
+
+								// 90度回転して垂直なベクトル軸
+								auto subposVerticalNorm = Vector3::Transform(subposNorm, Quaternion::CreateFromAxisAngle(rotAxis, XMConvertToRadians(90)));
+								auto subvelLength = subvel.Length();
+								// sin cosで長さを加味
+								auto subvelVertical = subposVerticalNorm * subvelLength * std::sin(rotAngle);
+								auto subvelHorizontal = subposNorm * subvelLength * std::cos(rotAngle);
+
+								// 速度に加算
+								auto w = 2;// obj1->m_objectWeight + obj2->m_objectWeight;
+								auto w1 = 1;// obj1->m_objectWeight / w;
+								auto w2 = 1;// obj2->m_objectWeight / w;
+								obj1->vel = -(subvelHorizontal * -.5f + subvelVertical * w1);
+								obj2->vel = +(subvelHorizontal * -.5f + subvelVertical * w2);
+								//obj1->m_objectPos += obj1->m_objectVel;
+								//obj2->m_objectPos += obj2->m_objectVel;
+								auto sradius = radius + radius;
+								auto subradius = subpos.Length() - sradius;
+								auto subadd = subposNorm * subradius * .55f;
+								obj1->transform->position += +subadd;
+								obj2->transform->position += -subadd;
+							}
+						}
+					}
+		}
+	};
 
 	class PlayerBehaviour : public GeometricObject
 	{
@@ -130,6 +192,7 @@ void BaseScene::Build(GameContext& context)
 	};
 
 	auto player = std::make_shared<PlayerBehaviour>(debugCamera);
+	player->layer = Layer::LAYER_PLAYER;
 	player->transform->localPosition = Vector3(0.f, 0.f, 5.f);
 	context << player;
 
